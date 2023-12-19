0131efa9e4170bb59c72b9235f30936942ef7104
Sai Lakshmi <srikakolapu-manasa.sai-lakshmi@capgemini.com>
2023-12-19 12:43:48 +0530

    ok

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Table, Row, Rows } from "react-native-table-component";
+import axios from "axios";
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [showData, setShowData] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setData(response.data);
+      setShowData(true);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const renderTable = () => {
+    if (showData) {
+      const tableHead = ["ID", "Title", "Price"];
+      const tableData = data.map((item) => [
+        item.id.toString(),
+        item.title,
+        item.price.toString(),
+      ]);
+
+      return (
+        <Table
+          borderStyle={{
+            borderWidth: 2,
+            borderColor: "black",
+            borderTopWidth: 80,
+          }}
+        >
+          <Row data={tableHead} style={styles.head} textStyle={styles.texth} />
+          <Rows data={tableData} textStyle={styles.text} />
+        </Table>
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Show Data" onPress={fetchData} />
+
+      {renderTable()}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
+    paddingTop: 30,
+  },
+  head: {
+    height: 40,
+    backgroundColor: "plum",
+  },
+  texth: {
+    margin: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  text: {
+    margin: 13,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
   },
 });
+
+export default App;
